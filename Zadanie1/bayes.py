26a474870a5f3c7238c4b179403b07ce0f84a34b
shreq <shrek.kresh@o2.pl>
2020-03-15 14:27:28 +0100

    bayes done

--- Zadanie1/bayes.py
+++ Zadanie1/bayes.py
@@ -1,19 +1,10 @@
 from sklearn import naive_bayes as nb
-import pandas
 
 from classifier import Classifier
 
 
 class Bayes(Classifier):
-    model = nb.GaussianNB()
 
-    def __init__(self, data, training_fraction=0.7):
+    def __init__(self, data, training_fraction=0.3):
         super().__init__(data, training_fraction)
-
-    def train(self):
-        self.model.fit(self.training_data,
-                       self.training_target_values)
-
-    def test(self):
-        prediction = self.model.predict(self.test_data)
-        print(prediction - self.test_target_values)
+        self.model = nb.GaussianNB()
