838701db659eb2abbcf0b4688a00a9a04444da51
PiotrekWardecki <piotrekwardecki2@gmail.com>
2020-03-23 21:12:25 +0100

    added hidden layer size in neural network algorithm

--- Zadanie1/classifiers/neuralNetwork.py
+++ Zadanie1/classifiers/neuralNetwork.py
@@ -1,5 +1,5 @@
 from sklearn import neural_network as nn
-
+from utils import clear
 from classifiers.classifier import Classifier
 
 
@@ -7,6 +7,10 @@ class NeuralNetwork(Classifier):
 
     name = "Artificial neural network algorithm"
 
+
     def __init__(self, data, lr, labels, training_fraction, arguments):
         super().__init__(data, lr, labels, training_fraction, arguments)
-        self.model = nn.MLPClassifier(max_iter=5000)
+        number_of_hidden_layer=int(input("Choose number of hidden_layers:"))
+        self.model = nn.MLPClassifier(alpha=0.0001,hidden_layer_sizes=
+                                      (number_of_hidden_layer,10),max_iter=5000)
+        
