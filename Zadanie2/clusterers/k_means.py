eed496c4f1a40d61d0c70d4dccacffdfc2dd93be
Karol Podlewski <podlewski@hotmail.com>
2020-04-13 14:18:03 +0200

    Added basics for k-means algorithm

--- Zadanie2/clusterers/k_means.py
+++ Zadanie2/clusterers/k_means.py
@@ -6,5 +6,14 @@ from clusterers.clusterer import Clusterer
 class Kmeans(Clusterer, KMeans):
     name = "K-means"
 
-    def __init__(self, data):
+    def __init__(self, data, n_clusters=8, n_init=10, max_iter= 300, random_state=None):
         super().__init__(data)
+        self.model = KMeans(
+            n_clusters=n_clusters,
+            n_init=n_init,
+            max_iter=max_iter,
+            random_state=random_state
+        )
+
+    def get_labels(self):
+        return self.model.labels_
