20a19f2f0a535c71e87d7d87c32bea42639260bd
Karol Podlewski <podlewski@hotmail.com>
2020-04-14 23:17:36 +0200

    Fixed script for kmeans

--- Zadanie2/scripts/kmeans.ps1
+++ Zadanie2/scripts/kmeans.ps1
@@ -1,15 +1,17 @@
 $FilePath = "kmeans.txt"
+$datasets = 4 
 
 "Losowanie centrow" >> $FilePath
 
-For ($i = 1; $i -le 3; $i++)
+For ($i = 1; $i -le $datasets; $i++)
 {
     "`nData set $i" >> $FilePath
 
     $array = @(1, 2, 5, 10, 20)
-    For ($j = 0; $j -le $array.length; $j++)
+    For ($j = 0; $j -lt $array.length; $j++)
     {
-        "$j`n" | Out-File $FilePath -Append -NoNewline
+        $var = $array[$j]
+        "$var`n" | Out-File $FilePath -Append -NoNewline
         python ./main.py -d $i -a 2 --fixed -p $array[$j] 300 None >> $FilePath
         "`n" | Out-File $FilePath -Append -NoNewline
     }
@@ -17,14 +19,15 @@ For ($i = 1; $i -le 3; $i++)
 
 "`nMaksymalne iteracje" >> $FilePath
 
-For ($i = 1; $i -le 3; $i++)
+For ($i = 1; $i -lt $datasets; $i++)
 {
     "`nData set $i" >> $FilePath
 
     $array = @(5, 10, 20, 25, 50, 100, 300, 600)
     For ($j = 0; $j -le $array.length; $j++)
     {
-        "$j`n" | Out-File $FilePath -Append -NoNewline
+        $var = $array[$j]
+        "$var`n" | Out-File $FilePath -Append -NoNewline
         python ./main.py -d $i -a 2 --fixed -p 10 $array[$j] None >> $FilePath
         "`n" | Out-File $FilePath -Append -NoNewline
     }
@@ -32,14 +35,15 @@ For ($i = 1; $i -le 3; $i++)
 
 "`nInicjalizacja centrow" >> $FilePath
 
-For ($i = 1; $i -le 3; $i++)
+For ($i = 1; $i -le $datasets; $i++)
 {
     "`nData set $i" >> $FilePath
 
     $array = @("None", 0, 42)
-    For ($j = 0; $j -le $array.length; $j++)
+    For ($j = 0; $j -lt $array.length; $j++)
     {
-        "$j`n" | Out-File $FilePath -Append -NoNewline
+        $var = $array[$j]
+        "$var`n" | Out-File $FilePath -Append -NoNewline
         python ./main.py -d $i -a 2 --fixed -p 10 300 $array[$j] >> $FilePath
         "`n" | Out-File $FilePath -Append -NoNewline
     }
