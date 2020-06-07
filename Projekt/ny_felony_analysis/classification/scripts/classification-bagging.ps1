16392646c32aeaee02c3c7215c1c7cbe55b4b671
Bartek Jurczewski <bartek.jurczewski@gmail.com>
2020-06-07 19:48:52 +0200

    [ADD] Boosting and bagging PS script to run

---
+++ Projekt/ny_felony_analysis/classification/scripts/classification-bagging.ps1
@@ -0,0 +1,30 @@
+$FilePath = "classification_boosting.txt"
+
+$label = @(3, 4, 5, 8, 9, 10, 11, 12, 13)
+For ($i = 0; $i -lt $label.Length; $i++)
+{
+    py ./main.py -ln $label[$i] -bg --time >> $FilePath
+
+$label = @(3, 4, 8, 9, 10, 11, 12, 13)
+For ($i = 0; $i -lt $label.Length; $i++)
+{
+    py ./main.py -ln $label[$i] -dn 2 5 -bg --time >> $FilePath
+}
+
+$label = @(5, 8, 9, 10, 11, 12, 13)
+For ($i = 0; $i -lt $label.Length; $i++)
+{
+    py ./main.py -ln $label[$i] -dn 3 4 -bg --time >> $FilePath
+}
+
+$label = @(3, 4, 5, 11, 12, 13)
+For ($i = 0; $i -lt $label.Length; $i++)
+{
+    py ./main.py -ln $label[$i] -dn 8 9 10 -bg --time >> $FilePath
+}
+
+$label = @(3, 4, 5, 8, 9, 10)
+For ($i = 0; $i -lt $label.Length; $i++)
+{
+    py ./main.py -ln $label[$i] -dn 11 12 13 -bg --time >> $FilePath
+}
---
+++ Projekt/ny_felony_analysis/classification/scripts/classification-boosting.ps1
@@ -0,0 +1,30 @@
+$FilePath = "classification_boosting.txt"
+
+$label = @(3, 4, 5, 8, 9, 10, 11, 12, 13)
+For ($i = 0; $i -lt $label.Length; $i++)
+{
+    py ./main.py -ln $label[$i] -bt --time >> $FilePath
+
+$label = @(3, 4, 8, 9, 10, 11, 12, 13)
+For ($i = 0; $i -lt $label.Length; $i++)
+{
+    py ./main.py -ln $label[$i] -dn 2 5 -bt --time >> $FilePath
+}
+
+$label = @(5, 8, 9, 10, 11, 12, 13)
+For ($i = 0; $i -lt $label.Length; $i++)
+{
+    py ./main.py -ln $label[$i] -dn 3 4 -bt --time >> $FilePath
+}
+
+$label = @(3, 4, 5, 11, 12, 13)
+For ($i = 0; $i -lt $label.Length; $i++)
+{
+    py ./main.py -ln $label[$i] -dn 8 9 10 -bt --time >> $FilePath
+}
+
+$label = @(3, 4, 5, 8, 9, 10)
+For ($i = 0; $i -lt $label.Length; $i++)
+{
+    py ./main.py -ln $label[$i] -dn 11 12 13 -bt --time >> $FilePath
+}
