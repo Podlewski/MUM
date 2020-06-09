851b9910c50326a0a41ad32038d32e5fa750bddd
Karol Podlewski <podlewski@hotmail.com>
2020-06-10 00:56:01 +0200

    [UPDATE] Layers order

--- Projekt/ny_felony_analysis/classification/extra_charts/charter.py
+++ Projekt/ny_felony_analysis/classification/extra_charts/charter.py
@@ -9,17 +9,16 @@ xlabels = ['Średnia Precyzja', 'Średnia Dokładność', 'Średnia Czułość']
 for file_name, charts_names in zip(files, charts):
     classifiers = pd.read_csv(file_name + '.csv', sep=';', header=0)
 
-    plt.grid(alpha=0.2)
-
     for index, row, in classifiers.iterrows():
         row_results = []
         row_results.append(row['Średnia Precyzja'])
         row_results.append(row['Średnia Dokładność'])
         row_results.append(row['Średnia Czułość'])
         
-        plt.plot(xlabels, row_results)
-        plt.scatter(x=xlabels, y=row_results, label=fill(row['Nazwa'], 15))
+        plt.plot(xlabels, row_results, zorder=3)
+        plt.scatter(x=xlabels, y=row_results, label=fill(row['Nazwa'], 15), zorder=6)
 
+    plt.grid(alpha=0.2, zorder=0)
     plt.xlabel('Metryka')
     plt.ylabel('Wartość')
     plt.title(charts_names)
@@ -33,15 +32,15 @@ for file_name, charts_names in zip(files, charts):
     plt.savefig(file_name + '.png', dpi=300)
     plt.close()
 
-    plt.grid(alpha=0.2)
 
     i = 0
     clas = len(classifiers.index)
     for index, row, in classifiers.iterrows():
-        plt.bar(i * clas, row['Średni Czas'], label=fill(row['Nazwa'], 15))
+        plt.bar(i * clas, row['Średni Czas'], label=fill(row['Nazwa'], 15), zorder=3)
         i = i + 0.15
 
     plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+    plt.grid(alpha=0.2, zorder=0)
     plt.xlabel('Średni czas')
     plt.ylabel('Czas (s)')
     plt.title(charts_names)
